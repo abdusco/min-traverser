01d7d7eb5d696a4f4762a28c08531181678e7d3a
Abdussamet Kocak <sssmmt@gmail.com>
2018-11-15 18:53:55 +0300

    Add comments

--- models/Tarjan.cpp
+++ models/Tarjan.cpp
@@ -7,9 +7,14 @@ Tarjan::Tarjan(const NodeList& nodes) : nodes(nodes) {}
 void Tarjan::sortTopological() {
     for (Node* node : nodes) {
         if (node->visited) continue;
+        // start with this node
         startingPoints.push_back(node);
+
+        // and explore as far as possible
         NodeList visited;
         depthFirstSearch(*node, visited);
+
+        // push visited nodes in reverse order
         for (Node* visitedNode : visited) {
             ordering.insert(ordering.begin(), visitedNode);
         }
